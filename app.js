5da180724fd3083cb682f7f3b4f16ce13671b157
Sruthii5 <sthukkani@hawk.iit.edu>
2020-09-14 22:14:03 -0500

    second  commit

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, 'view')));
 
-app.listen(8001, '0.0.0.0', function () { });
+app.listen(8002, '0.0.0.0', function () { });
 
 module.exports = app;
